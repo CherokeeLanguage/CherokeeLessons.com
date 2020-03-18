81e240a5a18bb950cf3e13d4cf93b7786bdc12a7
Michael Conrad <m.conrad.202@gmail.com>
2020-03-17 21:59:02 -0400

    try again with jekyll-menus via Gemfile 	modified:   Gemfile 	modified:   Gemfile.lock

--- jekyll-menus/lib/jekyll/menus.rb
+++
@@ -1,8 +0,0 @@
-require "jekyll/menus/version"
-
-module Jekyll
-  module Menus
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- jekyll-menus/lib/jekyll/menus/version.rb
+++
@@ -1,5 +0,0 @@
-module Jekyll
-  module Menus
-    VERSION = "0.1.0"
-  end
-end
