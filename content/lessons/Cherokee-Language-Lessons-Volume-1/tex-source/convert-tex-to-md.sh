1228c74309cc06ce8c65a392afa02d3496eb083d
Michael Conrad <m.conrad.202@gmail.com>
2020-03-26 11:17:48 -0400

    fix script to add required Zola header to output md file.

--- content/lessons/Cherokee-Language-Lessons-Volume-1/tex-source/convert-tex-to-md.sh
+++ content/lessons/Cherokee-Language-Lessons-Volume-1/tex-source/convert-tex-to-md.sh
@@ -12,5 +12,18 @@ perl -p -i -e 's/\\uline\{/\\emph\{/g' "$FILE".tex.tmp
 perl -p -i -e 's/\\label\{[^}]*?\}//g' "$FILE".tex.tmp
 perl -p -i -e 's/\\begin\{multicols\}.*$//g' "$FILE".tex.tmp
 perl -p -i -e 's/\\end\{multicols\}//g' "$FILE".tex.tmp
-pandoc -o "$FILE".md --to=commonmark --from=latex "$FILE".tex.tmp
+pandoc -o "$FILE".md.temp --to=commonmark --from=latex "$FILE".tex.tmp
+
+(
+echo "+++"
+echo "draft=true"
+echo "date = 2019-03-21"
+echo 'title = ""'
+echo "weight = 100000"
+echo "+++"
+echo 
+) > "$FILE".md
+cat "$FILE".md.temp >> "$FILE".md
+rm "$FILE".md.temp
+
 rm "$FILE".tex.tmp
