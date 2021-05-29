2196df43e7ab2059a03a45da7d096f2e6dda5d7a
Michael Conrad <m.conrad.202@gmail.com>
2021-05-29 18:07:23 -0400

    use correct tag values

--- content/content/cherokee-animal-names-tts-demo-audio-1/fix-mp3-tags.sh
+++ content/content/cherokee-animal-names-tts-demo-audio-1/fix-mp3-tags.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for x in *.mp3; do
-    y="$(echo $x|sed 's/walc-1_2021-..-.._00//g'|sed 's/.mp3//g')"
-    id3v2 -a "Michael Conrad" -g 101 -A "We are Learning Cherokee 1" -T "$y/31" -t "WALC 1 [$y]" -y 2021 "$x"
+    y="$(echo $x|sed 's/Animals_2021-..-.._00//g'|sed 's/.mp3//g')"
+    id3v2 -a "Michael Conrad" -g 101 -A "Animals [2021-05-29]" -T "$y/12" -t "Animals [$y]" -y 2021 "$x"
 done
 
